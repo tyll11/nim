35383e9528011d2602ca580e7870c3305097e96f
tyll11 <leung.theoyl@gmail.com>
2023-01-24 14:44:11 -0800

    all

--- Game.java
+++ Game.java
@@ -1,54 +1,98 @@
 import java.util.Scanner; 
+import java.util.Random;
 
 public class Game{
     public Game(){
 
     }
     Scanner a = new Scanner(System.in);
-    private int move = 0; 
+    Random rand = new Random();
+
+    private int move = rand.nextInt(1,2); 
+    private int player1Score = 0;
+    private int player2Score = 0;
     public Game(int s){
         startGame();
     }
     public void startGame(){
-        // welcome message
-       System.out.println("Welcome, say reset to reset at any time");
-        askMove();
+     
+  
+       System.out.println("How many pieces do you want the game to start with? Must be between 10 - 50");
+        int number = a.nextInt();
+        while(number > 50 || number < 10){
+            System.out.println("Not a valid number, try again.");
+            number = a.nextInt();
+        }
+            Board.populate(number);
+            askMove();
+      
+       
     }
 
+    public void playAgain(){
+        String answer = "";
+        System.out.println("Would you like to play again? yes or no");
+        answer = a.next();
+        // System.out.println(answer);
+        if(answer.equals("yes")){
 
+            startGame();
+        }
+    }
     public void askMove(){
         int userInput;
         // System.out.println(currentPlayer)
-        if(move % 2 == 0){
-            
-            System.out.print("Player 1's Move :" );
-            userInput = a.nextInt();
-            if( userInput <= .5 * Board.getPieces()){
-                Board.subtractPieces(userInput);
-                move += 1;
-                askMove();
+        if(Board.remainingPieces == 1){
+            if(move % 2 == 0){
+                player2Score++;
+                System.out.println("Player 2 Wins.");
+                System.out.println("Player 1: " + player1Score);
+                System.out.println("Player 2: " + player2Score);
+                playAgain();
             }
             else{
-                System.out.println("Not a valid input");
-                askMove();
-            }
-           
-            
+                player1Score++;
+                System.out.println("Player 1 Wins.");
+                System.out.println("Player 1: " + player1Score);
+                System.out.println("Player 2: " + player2Score);
+                playAgain();
 
+            }
         }
         else{
-            System.out.print("Player 2's Move :" );
-            userInput = a.nextInt();
-            if( userInput <= .5 * Board.getPieces()){
-                Board.subtractPieces(userInput);
-                move += 1;
-                askMove();
+            if(move % 2 == 0){
+                
+                System.out.print("Player 1's Move: " );
+                userInput = a.nextInt();
+                if( userInput <= .5 * Board.getPieces()){
+                    Board.subtractPieces(userInput);
+                    System.out.println("Remaining: " + Board.remainingPieces);
+                    move += 1;
+                    askMove();
+                }
+                else{
+                    System.out.println("Not a valid input");
+                    askMove();
+                }
+            
+                
+
             }
             else{
-                System.out.println("Not a valid input");
-                askMove();
+                System.out.print("Player 2's Move: " );
+                userInput = a.nextInt();
+                if( userInput <= .5 * Board.getPieces()){
+                    Board.subtractPieces(userInput);
+                    System.out.println("Remaining: " + Board.remainingPieces);
+                    move += 1;
+                    askMove();
+                }
+                else{
+                    System.out.println("Not a valid input");
+                    askMove();
+                }
             }
-        }
+    }
     }
 
 }
--- GameRunner.java
+++ GameRunner.java
@@ -1,9 +1,8 @@
 import java.util.Scanner; 
 public class GameRunner {
     public static void main(String[] args){
-        Scanner b = new Scanner(System.in);
-        System.out.println("How many pieces do you want the game to start with?");
-        Board.populate(b.nextInt());
+           // welcome message
+        System.out.println("Welcome");
         Game game = new Game();
         game.startGame();
     }
